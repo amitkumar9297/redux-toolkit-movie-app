edbdf5740715c24ac69f3f523995e6f1f838e091
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-26 16:48:54 +0530

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Userpic from '../../images/UserPic.jpeg'
+import './Header.scss'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className='header'>
+            <Link to={'/'}>
+                <div className='logo'>Movie App</div>
+            </Link>
+            <div className='user-image'>
+                <img src={Userpic} alt='user' />
+            </div>
+        </div>
     )
 }
 
