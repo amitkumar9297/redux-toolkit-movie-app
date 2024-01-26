6e18f290710de7c032dcb340de30fbd23810acaa
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-26 23:48:59 +0530

    movie and show add at home page ui and logic done

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { getAllMovies, getAllShows } from '../../features/movies/movieSlice'
+import MovieCard from '../MovieCard/MovieCard'
+
+import './MovieListing.scss'
 
 const MovieListing = () => {
+    // fetching movies
+    const movies = useSelector(getAllMovies);
+    let renderMovie = "";
+    renderMovie = movies.Response === 'True' ? (
+        movies.Search.map((movie, index) => (
+            < MovieCard key={index} data={movie} />
+        ))
+    ) : (
+        <div className="movie-error"><h3>{movies.Error}</h3></div>
+    );
+
+    // fetching shows
+    const shows = useSelector(getAllShows);
+    let renderShow = "";
+    renderShow = shows.Response === 'True' ? (
+        shows.Search.map((show, index) => (
+            < MovieCard key={index} data={show} />
+        ))
+    ) : (
+        <div className="movie-error"><h3>{shows.Error}</h3></div>
+    );
+    console.log(movies.Search)
     return (
-        <div>MovieListing</div>
+        <div className='movie-wrapper'>
+            <div className='movie-list'>
+                <h2>Movies</h2>
+                <div className='movie-container'>
+                    {renderMovie}
+                </div>
+            </div>
+            <div className='show-list'>
+                <h2>Shows</h2>
+                <div className='show-container'>
+                    {renderShow}
+                </div>
+            </div>
+        </div>
     )
 }
 
