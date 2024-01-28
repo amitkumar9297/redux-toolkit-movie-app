8ba4c1b7838185e596119bdfcd38600eb8dccdef
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-28 20:11:52 +0530

    some changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import { APIKey } from '../../common/apis/movieApiKey'
+import { useDispatch } from 'react-redux'
+import { addMovies, fetchAsyncMovies, fetchAsyncShows } from '../../features/movies/movieSlice'
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const movieText = "Harry";
+    const showText = "Friends"
+    useEffect(() => {
+        dispatch(fetchAsyncMovies(movieText));
+        dispatch(fetchAsyncShows(showText));
+
+    }, [dispatch])
+
     return (
-        <div>Home</div>
+        <div>
+            <div className='banner-img'></div>
+            <MovieListing />
+        </div>
     )
 }
 
 export default Home
+
+
+
+// const fetchMovies = async () => {
+//     const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+//         .catch((err) => {
+//             console.log(err);
+//         })
+
+//     dispatch(addMovies(response.data));
+// }
