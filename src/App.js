ca852800cbf4b892a80915999f97e9af1e2f0568
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-28 20:17:40 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom
 import Header from './components/Header/Header'
 import './App.scss';
 import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -12,9 +15,14 @@ function App() {
       <div className='app'>
         <Router>
           <Header />
-          <Routes>
-            <Route path='/' Component={Home} />
-          </Routes>
+          <div className='container'>
+            <Routes>
+              <Route path='/' exact Component={Home} />
+              <Route path='/movie/:imdbID' Component={MovieDetail} />
+              <Route path='/*' Component={PageNotFound} />
+            </Routes>
+          </div>
+          <Footer />
         </Router>
       </div>
     </>
